def94f2fd232090d392273b7a44d853985cc7386
Tom Dye <github@tomdye.co.uk>
2018-12-14 11:55:33 +0000

    adding typed-css-module generator and script for build material

---
+++ src/material/index.ts
@@ -0,0 +1,5 @@
+import * as textInput from './text-input.m.css';
+
+export default {
+	'@dojo/widgets/text-input': textInput
+};
---
+++ src/material/text-input.m.css.d.ts
@@ -0,0 +1,7 @@
+export const root: string;
+export const input: string;
+export const label: string;
+export const labelHasValue: string;
+export const hasTrailing: string;
+export const hasLeading: string;
+export const icon: string;
